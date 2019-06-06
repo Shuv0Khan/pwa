c2fdfdc389dfcfc5243dd6d9abc3e6a9993613c4
Shuvo <>
2019-06-07 00:42:26 +0600

    init

---
+++ sw.js
@@ -0,0 +1,31 @@
+
+const staticAssests = [
+  '../',
+  '../pwa/ShareX_Logo_1024.png',
+  '../pwa/style.css',
+  '../pwa/pwatest.js',
+];
+
+self.addEventListener('install', event=>{
+  event.waitUntil(
+    caches.open("sbc-static").then(cache=>{
+      return cache.addAll(staticAssests);
+    }).catch(error=>{
+      console.log(error);
+    }));
+});
+
+
+self.addEventListener('fetch', event=>{
+  const req = event.request;
+  event.respondWith(cacheFirst(req));
+});
+
+
+async function cacheFirst(req){
+  console.dir(req);
+
+  
+  const cacheMatch = await caches.match(req);
+  return cacheMatch || fetch(req);
+}
